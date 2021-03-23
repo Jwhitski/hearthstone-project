c9a15313e56ef6d7dca8aac7300b1fec3a5df4d8
Jason Whitlock <jrwhitlock7@gmail.com>
2021-03-22 18:57:53 -0700

    requiring gems and associating files

---
+++ environment.rb
@@ -0,0 +1,13 @@
+require 'bundler' #makes sure everything in gemfile is installed
+Bundler.require #require all gems listed in the gemfile
+require 'open-uri'
+require 'json'
+require 'openssl'
+
+
+
+#requiring files that lives in the same repo folder / files can 'talk' to eachother.
+require_relative "./lib/api"
+require_relative "./lib/cli"
+require_relative "./lib/cards"
+require_relative "./lib/card_set"
