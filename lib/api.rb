8d208aef26e11e7db709e372150b6cb7d9fe7709
Jason Whitlock <jrwhitlock7@gmail.com>
2021-03-24 17:52:54 -0700

    added methods to get card sets and all cards

--- lib/api.rb
+++ lib/api.rb
@@ -1,11 +1,11 @@
 class API
-
+    
     def self.run
         # url = "https://omgvamp-hearthstone-v1.p.rapidapi.com/?rapidapi-key=#{api_key}/cards"
         # uri = URI(url)
         url = URI("https://omgvamp-hearthstone-v1.p.rapidapi.com/cards")
 
-        # response = Net::HTTP.get(uri) #'gets' info from a website.
+        # response = Net::HTTP.get(uri) #'gets' info from a we@setsite.
         http = Net::HTTP.new(url.host, url.port)
         http.use_ssl = true
         http.verify_mode = OpenSSL::SSL::VERIFY_NONE
@@ -16,23 +16,29 @@ class API
 
         response = http.request(request)
         # puts response.read_body
-        hash = JSON.parse(response.read_body) #turns string into JSON. "hash" {...} hash with the key of basic. which points to 3 arrays for value.
-        array_of_sets = hash.keys
-        array_of_sets.each do |set|
-            card_set_instance = CardSet.new(name: set) #List of all set names.
-        end 
+        @sets = JSON.parse(response.read_body) #turns string into JSON. "hash" {...} hash with the key of basic. which points to 3 arrays for value.
+        binding.pry
+    end
 
-        hash.each do |key, deck|
+    def get_sets
+        @sets.keys # returns the sets. ["Basic", "Classic", etc...]
+    end
+
+
+    def self.get_decks #grabs the array of cards / this should keep track of cards
+        @all_cards = [] #stores all cards / decks...
+        @sets.each do |key, deck|
             if deck.kind_of?(Array) # Makes sure it is an array we are getting before iterating
                 deck.each do |card| # Iterate over the deck
-                    card_instance = Cards.new(name: card["name"], text: card["text"])
-                    card_instance.cost = card["cost"]
-                    card_instance.attack = card["attack"]
-                    card_instance.health = card["health"]
-                    card_instance.img = card["img"]
+                    @all_cards.push(card)
                 end
             end
         end
-
     end
+
+    
+    # def get_card(user_input_card)
+    # end
+    
+    
 end
