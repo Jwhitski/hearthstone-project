9ca8b1fb31a8a18ad90ce544a860a1f71eb2fa6d
Jason Whitlock <jrwhitlock7@gmail.com>
2021-03-28 11:40:40 -0700

    changed class folders / no longer needed methods

--- lib/api.rb
+++ lib/api.rb
@@ -5,7 +5,7 @@ class API
         # uri = URI(url)
         url = URI("https://omgvamp-hearthstone-v1.p.rapidapi.com/cards")
 
-        # response = Net::HTTP.get(uri) #'gets' info from a we@setsite.
+        # response = Net::HTTP.get(uri) #'gets' info from a website.
         http = Net::HTTP.new(url.host, url.port)
         http.use_ssl = true
         http.verify_mode = OpenSSL::SSL::VERIFY_NONE
@@ -14,31 +14,11 @@ class API
         request["x-rapidapi-key"] = ENV["HEARTHSTONE_KEY"]
         request["x-rapidapi-host"] = 'omgvamp-hearthstone-v1.p.rapidapi.com'
 
-        response = http.request(request)
+        puts 'Loading card data...'.colorize(:blue)
+
+        response = http.request(request) 
+
         # puts response.read_body
-        @sets = JSON.parse(response.read_body) #turns string into JSON. "hash" {...} hash with the key of basic. which points to 3 arrays for value.
-        binding.pry
-    end
-
-    def get_sets
-        @sets.keys # returns the sets. ["Basic", "Classic", etc...]
-    end
-
-
-    def self.get_decks #grabs the array of cards / this should keep track of cards
-        @all_cards = [] #stores all cards / decks...
-        @sets.each do |key, deck|
-            if deck.kind_of?(Array) # Makes sure it is an array we are getting before iterating
-                deck.each do |card| # Iterate over the deck
-                    @all_cards.push(card)
-                end
-            end
-        end
-    end
-
-    
-    # def get_card(user_input_card)
-    # end
-    
-    
+        JSON.parse(response.read_body) #turns string into JSON. "hash" {...} 
+    end    
 end
