c60ed83e4abc08285fda3aa4550d73aa436e5ea5
Jason Whitlock <jrwhitlock7@gmail.com>
2021-03-28 11:43:58 -0700

    specific card class to help display attributes

---
+++ lib/card.rb
@@ -0,0 +1,31 @@
+class Card
+    # attr_accessor :name, :text, :cost, :attack, :health, :img
+
+    def initialize(card_data)
+        @name = card_data["name"]
+        @text = card_data["text"]
+        @cost = card_data["cost"]
+        @attack = card_data["attack"]
+        @health = card_data["health"]
+        @img = card_data["img"]
+    end
+
+    def get_name
+        @name
+    end
+
+
+    def show
+        puts @name.colorize(:light_blue)
+        puts "-------------------------".colorize(:light_blue)
+        puts @text
+        puts "-------------------------".colorize(:light_blue)
+        
+        
+        puts "Cost: #{@cost}".colorize(:blue) 
+        puts "Attack: #{@attack}".colorize(:red)
+        puts "Health: #{@health}".colorize(:yellow)
+        puts "Image: #{@img}"
+        puts "Note: Not all cards will have all attributes."
+    end
+end
