a1a010779f0941efbe85d4f1cdba0363961f1b58
Jason Whitlock <jrwhitlock7@gmail.com>
2021-03-28 12:49:24 -0700

    added comments and a choose again option

--- lib/cli.rb
+++ lib/cli.rb
@@ -7,7 +7,7 @@ class CLI
         @sets = Array.new
         api_data = API.run
         api_data.each do |set, card_data|
-            @sets.push(Set.new(set, card_data))
+            @sets.push(Set.new(set, card_data)) #instantiates set and card classes / saves in @sets
         end
         self.menu_set
     end
@@ -19,16 +19,16 @@ class CLI
     end
 
     def card_set_list #displays the sets... indexed 1-34
-        @sets.each.with_index(1) do |set, index|
-            puts "#{index}. #{set.get_name}"
+        @sets.each.with_index(1) do |set, index| #stores each instance of sets
+            puts "#{index}. #{set.get_name}" #calls Set.get_name
         end
 
         puts "Type the number corresponding to the set you would like to view:"
         set_input = gets.to_i #user selects their card... if user selects 1.
 
         puts "Here are the cards for that set"
-        @chosen_set = @sets[set_input - 1]
-        @chosen_set.list_cards
+        @chosen_set = @sets[set_input - 1] #stores selection
+        @chosen_set.list_cards #Set.list_cards
         
         self.menu_card
     end
@@ -36,10 +36,20 @@ class CLI
     def menu_card
         puts "Which card would you like to see from #{@chosen_set.get_name}?"
         card_input = gets.to_i
-        @chosen_card = @chosen_set.get_card(card_input)
-        @chosen_card.show
+        @chosen_card = @chosen_set.get_card(card_input) #Set.get_card
+        @chosen_card.show #Card.show
+        self.choose_again
     end
 
+    def choose_again
+        puts "Would you like to choose again? Enter 'yes' to restart program, or any other key to exit."
+        input = gets.strip.downcase
+        if input == "yes"
+            self.menu_set
+        else
+            puts 'Thank you for using the Hearthstone CLI.'
+        end
+    end
 
     
 end
