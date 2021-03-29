97f64891c1220e066462418a7090b41a4ecb5cd1
Jason Whitlock <jrwhitlock7@gmail.com>
2021-03-28 19:32:51 -0700

    added comments

--- lib/set.rb
+++ lib/set.rb
@@ -1,13 +1,13 @@
 class Set
     def initialize(name, cards)
         @name = name #name of card sets
-        @cards = [] #Array of card values / decks
+        @cards = [] #Array of card instances / objects
         self.load_cards(cards)
     end
 
     def load_cards(card_data) 
         card_data.each do |card|#iterates through each nested array
-            @cards.push(Card.new(card))
+            @cards.push(Card.new(card)) #Saves each instance of 'card' into @cards array
         end
     end
 
@@ -15,7 +15,7 @@ class Set
         @name
     end
 
-    def list_cards
+    def list_cards #@chosen_set.list_cards -- takes the key called and lists cards for that key
         @cards.each.with_index(1) do |card, index| #goes through each card instance and grabs names
             puts "#{index}. #{card.get_name}"
         end
